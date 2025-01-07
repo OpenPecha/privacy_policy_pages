47a23540ac577845b6189ff7620a8ebf80d7a661
tenkus47 <tenkus47@gmail.com>
2025-01-07 12:05:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import { HashRouter, Route, Routes, useParams } from 'react-router-dom';
 import Policy from './components/Privacy_Policy';
 
 function App() {
   return (
-    <Router basename={'/privacy_policy_pages'}>
+    <HashRouter>
     <Routes>
-      <Route exact path="/privacy_policy_pages" element={<div >hi</div>} />
-      <Route path="/:dynamicName" element={<DynamicPage />} />
+      <Route exact path="/privacy_policy_pages/" element={<div >hi</div>} />
+      <Route path="/privacy_policy_pages/:dynamicName" element={<DynamicPage />} />
     </Routes>
-  </Router>
+  </HashRouter>
   );
 }
 
