0a6fe674292c0d34cb3049668c78ea8bb7edcf35
tenkus47 <tenkus47@gmail.com>
2025-01-07 17:02:56 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { HashRouter, Routes,Route } from "react-router-dom";
 import App from './App.tsx';
 import Policy from './components/Privacy_Policy.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/'>
+    <HashRouter >
     <Routes>
         <Route  path="/" element={<App />} />
         <Route path="/:productName" element={<Policy />} />
       </Routes>
-   </BrowserRouter>
+   </HashRouter>
   </StrictMode>,
 )
