7302a5654f4c6dfaca55e537d9aa91debe432756
tenkus47 <tenkus47@gmail.com>
2025-01-07 15:48:44 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/privacy_policy_pages/', // Use the subdirectory for deployment
+  base: '/', // Use the subdirectory for deployment
   plugins: [react()],
 });
